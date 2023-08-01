7a3c20752b0c5bb9811673e2bcfa4d6655ccd285
Alex Durrant <alexjdurrant@gmail.com>
2023-08-01 16:30:45 -0600

    completed the login function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const SECRET = process.env.SECRET
+const {User} = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 const createToken = (username, id) => {
     return jwt.sign(
@@ -17,13 +19,65 @@ const createToken = (username, id) => {
 
 module.exports = {
     login: async (req, res) => {
-        console.log('login')
-        let {username, password} = req.body
-        let token = createToken(username, password)
-        res.status(200).send(token)
+        try{
+            const {username, password} = req.body
+            let foundUser = await User.findOne({where: {username: username}})
+
+            if (!foundUser){
+                res.status(400).send('Invalid entry. Try again')
+            } else {
+
+                const isAuthenticated = bcrypt.compareSync(password, foundUser.hashedPass)
+
+                if (isAuthenticated){
+                    let token = createToken(foundUser.dataValues.username, foundUser.dataValues.id)
+                    const exp = Date.now() + 1000 * 60 * 60 * 48
+    
+                    const data = {
+                        username: foundUser.dataValues.username,
+                        userId: foundUser.dataValues.id,
+                        token: token,
+                        exp: exp
+                    }
+                    res.status(200).send(data)
+                } else {
+                    res.status(400).send('Invalid Password')
+                }
+            }
+        }
+        catch (error) {
+            console.log(error)
+            res.status(400).send(error)
+        }
     },
     register: async (req,res) => {
-        console.log('register')
-        res.status(200)
+        try{
+            const {username, password} = req.body
+            let foundUser = await User.findOne({where: {username: username}})
+
+            if (foundUser){
+                res.status(400).send('User already exists try a different name')
+            } else {
+                const salt = bcrypt.genSaltSync(10)
+                const hash = bcrypt.hashSync(password, salt)
+
+                let newUser = await User.create({username: username, hashedPass: hash})
+
+                let token = createToken(newUser.dataValues.username, newUser.dataValues.id)
+                const exp = Date.now() + 1000 * 60 * 60 * 48
+
+                const data = {
+                    username: newUser.dataValues.username,
+                    userId: newUser.dataValues.id,
+                    token: token,
+                    exp: exp
+                }
+                res.status(200).send(data)
+            }
+        }
+        catch (error) {
+            console.log(error)
+            res.status(400).send(error)
+        }
     }
 }
--- server/index.js
+++ server/index.js
@@ -3,23 +3,32 @@ const cors = require('cors')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
+const {sequelize} = require('./util/database')
+const {User} = require('./models/user')
+const {Post} = require('./models/post')
 
 app.use(express.json())
 app.use(cors())
 
+User.hasMany(Post)
+Post.belongsTo(User)
+
 const {login, register} = require('./controllers/auth.js')
 const {addPost, getAllPosts, getCurrentUserPosts, editPost, deletePost} = require('./controllers/posts.js')
+const {isAuthenticated} = require('./middleware/isAuthenticated.js')
+
 
 app.post('/register', register)
 app.post('/login', login)
-app.post('/posts', addPost)
+app.post('/posts', isAuthenticated, addPost)
 
 app.get('/posts', getAllPosts)
 app.get('/userposts/:userId', getCurrentUserPosts)
 
-app.put('/posts/:id', editPost)
+app.put('/posts/:id', isAuthenticated, editPost)
 
-app.delete('/posts/:id', deletePost)
+app.delete('/posts/:id',isAuthenticated, deletePost)
 
-
-app.listen(PORT, () => console.log(`Running on Port ${PORT}`))
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log(`Running on Port ${PORT}`)) 
+}).catch(err => console.log(err))
