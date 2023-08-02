3450a950aee776aab8429eb06a098872c32a1e73
Alex Durrant <alexjdurrant@gmail.com>
2023-08-02 16:28:58 -0600

    finished the optional challenge

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -6,16 +6,27 @@ const Auth = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [register, setRegister] = useState(true);
+  const [counter, setCounter] = useState(0);
   const { dispatch } = useContext(AuthContext);
 
   const submitHandler = (e) => {
+    const responses = ['Incorrect', 'Incorrect again', 'This is your third attempt.. you better get this one right','bruh..... nice try!!']
+    
     e.preventDefault();
     const body = { username, password };
     axios.post(register ? "/register" : '/login', body )
-      .then((res) => {
-        dispatch({ type: "LOGIN", payload: res.data })
-      })
-      .catch(err => console.log(err))
+    .then((res) => {
+      dispatch({ type: "LOGIN", payload: res.data })
+    })
+    .catch((err) => {
+      console.log(err)
+      if (counter === responses.length){
+         return setCounter(0)
+      }
+      alert(`${responses[counter]}`)
+      let newCounter = counter + 1 
+      setCounter(newCounter)
+    })
     
     console.log("submitHandler called");
   };
